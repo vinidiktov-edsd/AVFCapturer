9294ec940b5db977b158e10ccc4a096d8064f00e
Alexei Vinidiktov <vinidiktov@edsd.com>
2017-11-03 10:18:45 +0700

    Initial commit

---
+++ AVFCapturer/CameraEngine.h
@@ -0,0 +1,13 @@
+//
+//  CameraEngine.h
+//  AVFCapturer
+//
+//  Created by Admin on 03/11/17.
+//  Copyright © 2017 Edison Software Development. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CameraEngine : NSObject
+
+@end
---
+++ AVFCapturer/CameraEngine.m
@@ -0,0 +1,13 @@
+//
+//  CameraEngine.m
+//  AVFCapturer
+//
+//  Created by Admin on 03/11/17.
+//  Copyright © 2017 Edison Software Development. All rights reserved.
+//
+
+#import "CameraEngine.h"
+
+@implementation CameraEngine
+
+@end
